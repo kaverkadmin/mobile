ee00db5caccec5a32101c2024fb8b658e0a1d50a
Huseyin Bedir <57250424+huseyinbedir@users.noreply.github.com>
2022-03-30 18:42:57 +0300

    30.03.2022
    Core Edit

---
+++ kaverka_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(KaverkaApp());
+
+class KaverkaApp extends StatelessWidget {
+  const KaverkaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
